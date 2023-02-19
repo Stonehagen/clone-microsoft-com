bbb8ea184d59947a8ba28dfbb489400dc65ef908
Stonehagen <steinhagen@mailbox.org>
2023-02-19 23:22:39 +0100

    layout rough structure of HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React from 'react';
+import './styles/App.css';
 
 const App = () => {
   return (
     <div className="App">
-     
+      <div className="Header">HEADER MENU</div>
+      <div className="Main">
+        <div className="Carousel-Wide">Carousel</div>
+        <div className="Shop-List">Buy Stuff</div>
+        <div className="News-Grid">News</div>
+        <div className="Xbox-Hero">XBOX</div>
+        <div className="News-Grid-Business">Business News</div>
+        <div className="Carousel-Small">Carousel</div>
+        <div className="Social-List"></div>
+        <button>Back to top</button>
+      </div>
+      <div className="Footer">FOOTER TEXT</div>
     </div>
   );
-}
+};
 
 export default App;
