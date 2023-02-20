b79588b20cc1877452611f1a945a9480ac4f3be3
Stonehagen <steinhagen@mailbox.org>
2023-02-20 11:37:47 +0100

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import '../styles/Header.css';
 import Logo from '../img/logo.png';
-import {ReactComponent as Search } from '../img/search.svg'
-import {ReactComponent as Cart } from '../img/cart.svg'
-import {ReactComponent as Profile } from '../img/profile.svg'
+import { ReactComponent as Search } from '../img/search.svg';
+import { ReactComponent as Cart } from '../img/cart.svg';
+import { ReactComponent as Profile } from '../img/profile.svg';
+import { ReactComponent as DropDown } from '../img/dropDown.svg';
 
 const Header = () => {
   return (
     <div className="Header">
-      <img src={Logo} alt="Microsoft" />
-      <ul>
-        <li>Microsoft 365</li>
-        <li>Teams</li>
-        <li>Windows</li>
-        <li>Surface</li>
-        <li>Xbox</li>
-        <li>Deals</li>
-        <li>Small Business</li>
-        <li>Support</li>
-      </ul>
-      <ul>
-        <li>All Microsoft</li>
-        <li><Search /></li>
-        <li><Cart /></li>
-        <li><Profile /></li>
+      <div className="Header-Nav-Left">
+        <img src={Logo} alt="Microsoft" className="logo" />
+        <ul>
+          <li>Microsoft 365</li>
+          <li>Teams</li>
+          <li>Windows</li>
+          <li>Surface</li>
+          <li>Xbox</li>
+          <li>Deals</li>
+          <li>Small Business</li>
+          <li>Support</li>
+        </ul>
+      </div>
+      <ul className="Header-Nav-Right">
+        <li className="All-Microsoft">
+          <span>All Microsoft</span>
+          <DropDown />
+        </li>
+        <li>
+          <Search />
+        </li>
+        <li>
+          <Cart />
+        </li>
+        <li>
+          <Profile />
+        </li>
       </ul>
     </div>
   );
